d8ad94cf5c21f66ca4ba9411ba55a3f94edb245f
Lucas Cardoso <mr.lucascardoso@gmail.com>
2017-07-06 15:06:49 -0300

    conf for publish

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'PyZenvia',
+  packages = ['pyzenvia'], # this must be the same as the name above
+  version = '0.1',
+  description = 'package for send sms by Zenvia API',
+  author = 'Lucas Cardoso',
+  author_email = 'mr.lucascardoso@gmail.com',
+  url = 'https://github.com/mrlucascardoso/pyzenvia',
+  # download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['sms', 'zenvia'], # arbitrary keywords
+  classifiers = [],
+)
